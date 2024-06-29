37869fca345fce46ee78320e7d85e85dca4ae8f7
ebuddenberg <buddenbergev@gmail.com>
2024-06-29 09:39:45 -0700

    Update the classname

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-	let dropdown_class = 'dropdown-menu';
-	let bs_toggle = 'dropdown';
+	let dropdown_class = 'oc-dropdown-menu';
+	let bs_toggle = 'oc-dropdown';
 	let bs_target = 'data-bs-auto-close="outside"';
 	let aria_control = '';
 	let padding_start = 'ps-3';
-	let dropdown_toggle_class = 'dropdown-toggle';
-	document.getElementById('main_menu').innerHTML = '<li>Loading Menu..</li>';
+	let dropdown_toggle_class = 'oc-dropdown-toggle';
+	document.getElementById('oc_main_menu').innerHTML = '<li>Loading Menu..</li>';
 fetch('https://oc-api.com/api/nav/items',{
     method: 'GET',
     headers: {
@@ -20,8 +20,8 @@ fetch('https://oc-api.com/api/nav/items',{
 		let str = '';
 		groups.forEach(function(item) {
 			if(window.screen.width < 981){
-				dropdown_class = 'dropdown-menu dropdown-menu-mobile accordion-collapse collapse';
-				bs_toggle = 'collapse';
+				dropdown_class = 'oc-dropdown-menu oc-dropdown-menu-mobile oc-accordion-collapse oc-collapse';
+				bs_toggle = 'oc-collapse';
 				bs_target = 'data-bs-target="#accordion_'+item.id+'"';
 				aria_control = 'aria-controls="accordion_'+item.id+'"';
 				padding_start = '';
@@ -30,18 +30,19 @@ fetch('https://oc-api.com/api/nav/items',{
 				bs_toggle = '';
 				dropdown_toggle_class = '';
 			}
-		  str += '<li class="nav-item dropdown"><a  title="'+item.description+'" class="navlink nav-link '+dropdown_toggle_class+'" href="#" role="button" data-bs-toggle="'+bs_toggle+'" '+bs_target+' aria-expanded="false" '+aria_control+' data-gid="'+item.id+'">'+ item.title + '</a>';
+		  str += '<li class="oc-nav-item oc-dropdown"><a title="'+item.description+'" class="oc-tooltip oc-navlink oc-nav-link '+dropdown_toggle_class+'" href="#" role="button" data-bs-toggle="'+bs_toggle+'" '+bs_target+' aria-expanded="false" '+aria_control+' data-gid="'+item.id+'">'+ item.title + '<span class="oc-tooltip-text">' + item.description +'</span></a>';
+
 		  if(item.subGroups.length > 0){
 			  const subGroup = item.subGroups;
-			  str += '<ul id="accordion_'+item.id+'" class="tgsz '+dropdown_class+'"><div class="row">';
+			  str += '<ul id="accordion_'+item.id+'" class="tgsz '+dropdown_class+'"><div class="oc-row">';
 			  subGroup.forEach(function(gItem) {
 				  let groupTitle = gItem.title;
-				  str += '<div class="col">'
-					  + '<li  class="'+padding_start+' border-bottom pb-1 mb-1 submenu-lvl1">'+groupTitle.toUpperCase()+' <i class="bi bi-caret-down-fill float-end pe-3 show"></i><i class="bi bi-caret-up-fill float-end pe-3"></i></li><ul class="submenu-lvl2">';
+				  str += '<div class="oc-col">'
+					  + '<li  class="'+padding_start+' border-bottom oc-pb-1 oc-mb-1 submenu-lvl1">'+groupTitle.toUpperCase()+' <i class="bi bi-caret-down-fill oc-float-end oc-pe-3 show"></i><i class="bi bi-caret-up-fill oc-float-end oc-pe-3"></i></li><ul class="submenu-lvl2">';
 				  if(gItem.items.length > 0){
 					 const items = gItem.items;
 					  items.forEach(function(lnk) {
-						  str += '<li><a title="'+lnk.description+'" class="dropdown-item" href="'+lnk.url+'">'+lnk.title+'</a></li>';
+						  str += '<li><a title="'+lnk.description+'" class="oc-dropdown-item oc-tooltip" href="'+lnk.url+'">'+lnk.title+'<span class="oc-tooltip-text">' + lnk.description +'</span></a></li>';
 					  }); 
 				  }
 				  str += '</div>';
@@ -50,12 +51,12 @@ fetch('https://oc-api.com/api/nav/items',{
 		  }
 		  str += '</li>';
 		});
-		document.getElementById('main_menu').innerHTML = str;
+		document.getElementById('oc_main_menu').innerHTML = str;
 
 
 
-		const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-		const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+		const dropdownToggles = document.querySelectorAll('.oc-dropdown-toggle');
+		const dropdownMenus = document.querySelectorAll('.oc-dropdown-menu');
 
 		function handleMouseOver(event) {
 			event.preventDefault();
@@ -132,7 +133,7 @@ fetch('https://oc-api.com/api/nav/items',{
 		}
 
 		window.addEventListener('click', function(event) {
-			if (!event.target.matches('.dropdown-toggle')) {
+			if (!event.target.matches('.oc-dropdown-toggle')) {
 				closeAllDropdowns();
 			}
 		});
@@ -140,8 +141,8 @@ fetch('https://oc-api.com/api/nav/items',{
 
 		
 
-		const dropdownToggle = document.querySelector('#hamburger_menu .navbar-toggler');
-		const dropdownMenu = document.getElementById('navbarTogglerDemo02');
+		const dropdownToggle = document.querySelector('#oc_hamburger_menu .oc-navbar-toggler');
+		const dropdownMenu = document.getElementById('oc-navbarTogglerDemo02');
 
 		dropdownToggle.addEventListener('click', function() {
 			dropdownMenu.classList.toggle('show');
@@ -163,15 +164,15 @@ fetch('https://oc-api.com/api/nav/items',{
 
 
 
-				$(this).parents('.dropdown-menu-mobile.dropdown-menu').addClass('showMobile');
+				$(this).parents('.oc-dropdown-menu-mobile.oc-dropdown-menu').addClass('showMobile');
 
 
 			});
 
 			$('.navlink').click(function(){
 
-				var mHasxClass = $(this).siblings('.dropdown-menu-mobile');
-				var mHasShowClass = $(this).siblings('.dropdown-menu-mobile.showMobile');
+				var mHasxClass = $(this).siblings('.oc-dropdown-menu-mobile');
+				var mHasShowClass = $(this).siblings('.oc-dropdown-menu-mobile.showMobile');
 
 				if(mHasShowClass.hasClass('showMobile')) {
 					mHasShowClass.removeClass('show');
@@ -189,13 +190,26 @@ fetch('https://oc-api.com/api/nav/items',{
 
 				}
 
-				$(this).parent().siblings().find('.dropdown-menu-mobile').removeClass('show showMobile');
-				$(this).parent().siblings().find('.dropdown-menu-mobile').find('.submenu-lvl2').removeClass('show');
+				$(this).parent().siblings().find('.oc-dropdown-menu-mobile').removeClass('show showMobile');
+				$(this).parent().siblings().find('.oc-dropdown-menu-mobile').find('.submenu-lvl2').removeClass('show');
 			});
 
 
 
 		});
+
+		$(document).ready(function() {
+			$('.oc-tooltip').hover(
+				function() {
+					$(this).find('.oc-tooltip-text').addClass('show');
+				},
+				function() {
+					$(this).find('.oc-tooltip-text').removeClass('show');
+				}
+			);
+		});
+
+
 	});
   
 }
@@ -203,26 +217,26 @@ fetch('https://oc-api.com/api/nav/items',{
 window.onresize = function(event) {
 	let dropdown = document.querySelectorAll('.tgsz');
 	let navlink = document.querySelectorAll('.navlink');
-	let search_box = document.getElementById('search_box');
+	let search_box = document.getElementById('oc_search_box');
 	
     if(window.screen.width > 980){// big screen
-		search_box.classList.add('d-flex');
+		search_box.classList.add('oc-d-flex');
 		for (var i = 0; i < navlink.length;  i++) {		
 			if (typeof dropdown[i] !== 'undefined'){
-				dropdown[i].classList.add('dropdown-menu');
-				dropdown[i].classList.remove('accordion-collapse','collapse','show');
-				navlink[i].setAttribute('data-bs-toggle', 'dropdown');
+				dropdown[i].classList.add('oc-dropdown-menu');
+				dropdown[i].classList.remove('oc-accordion-collapse','oc-collapse','show');
+				navlink[i].setAttribute('data-bs-toggle', 'oc-dropdown');
 			}
 		}
 		
 	}else{
-		search_box.classList.remove('d-flex');
+		search_box.classList.remove('oc-d-flex');
 		for (var j = 0; j < navlink.length;  j++) {
 			let gid = navlink[j].getAttribute('data-gid');
 			if (typeof dropdown[j] !== 'undefined'){
-				dropdown[j].classList.add('accordion-collapse','collapse');
-				dropdown[j].classList.remove('dropdown-menu');
-				navlink[j].setAttribute('data-bs-toggle', 'collapse');
+				dropdown[j].classList.add('oc-accordion-collapse','oc-collapse');
+				dropdown[j].classList.remove('oc-dropdown-menu');
+				navlink[j].setAttribute('data-bs-toggle', 'oc-collapse');
 			}
 			
 			navlink[j].setAttribute('data-bs-target', '#accordion_'+gid);
@@ -230,3 +244,4 @@ window.onresize = function(event) {
 		}	
 	}
 };
+
